3604f2aba32e6fbfd6de59f86e8eeed4845696d0
gopinadh <kgopinadh714316@gmail.com>
2021-11-25 04:23:28 +0530

    method and gate application

---
+++ Gopi/methodes.java
@@ -0,0 +1,25 @@
+ class CostumerName {
+public static void main(String[] args){
+System.out.println("gopinadh");
+}
+
+} 
+
+
+class CostumerName1{
+
+public static void main(String[] args){
+System.out.println("srinu");
+}
+
+}
+
+
+class CostumerName3{
+
+public static void main(String[] args){
+System.out.println("anil");
+}
+
+
+}
