920b8dc8f6ce413f3c6097cc90c89cc112b9f883
venkateshasul1 <94854961+venkateshasul1@users.noreply.github.com>
2021-11-25 21:56:30 +0530

    Create GmailForm.java

---
+++ Venkatesh/GmailForm.java
@@ -0,0 +1,13 @@
+class GmailForm{
+  String firstName="venkatesh";
+  String lastName="asula";
+  String userName="asulavenkatesh02";
+  String passWord="venkat@0401";
+  String conformPassWord="venkat@0401";
+  long phoneNumber="8985185017";
+  char gender='m';
+  boolean physicallyDisabled=false;
+  
+}
+
+}
