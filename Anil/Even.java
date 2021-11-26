b9670e8a511ef7498a501cb923494b95482c2178
anil4java <ullianil4java@gmail.com>
2021-11-26 18:51:28 +0530

    check even or add and if & else condition practice

---
+++ Anil/Even.java
@@ -0,0 +1,12 @@
+class EvenOdd{
+public static void main(String args[]){
+    int num = 55;
+
+   if (num%2 ==0){
+  System.out.println(" number is even");
+  }
+   else{
+   System.out.println("number is odd");
+}
+}
+}
---
+++ Anil/Student.java
@@ -0,0 +1,11 @@
+class Student{
+public static void main (String args[]){
+int num =100;
+if (num ==100){
+System.out.println(" excute a statement");
+}
+else{
+System.out.println(" not excute a statement");
+}
+}
+}
