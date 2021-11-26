540a0cb6a840bb2827da39429e3a5ea26d35d86d
anil4java <ullianil4java@gmail.com>
2021-11-26 18:15:31 +0530

    file adding fx class

---
+++ Anil/anil.java
@@ -0,0 +1,5 @@
+class Fx{
+public static void main(String args[]){
+  System.out.println("i am very good boy");
+}
+}
---
+++ Anil/govardhan.java
@@ -0,0 +1,17 @@
+ class Student {
+	int id;
+	String name;
+	
+	
+}
+
+class TestStudent {
+	public static void main (String args[]) {
+		Student s = new Student();
+                  s.id = 101;
+                  s.name="anil";
+		System.out.println(s.id);
+		System.out.println(s.name);
+		
+	}
+}
