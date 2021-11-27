81e4bf4b9a3e5b70d479d4a1b9cb17638394ccbb
anil4java <ullianil4java@gmail.com>
2021-11-27 20:32:52 +0530

    27-11-2021 practice if & else condition check vowel and consonant

---
+++ Anil/VowelConsonant.java
@@ -0,0 +1,12 @@
+    class VowelConsonant{
+     public static void main(String args[]){
+      char ch = 'u';
+      if(ch =='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+       System.out.println(ch + "is a vowel");
+      }
+     else{
+      System.out.println(ch + "is a consonant");
+     }
+
+}
+}
---
+++ Anil/VowelConsonantEx.java
@@ -0,0 +1,12 @@
+class VowelConsonantEx{
+   public static void main(String args[]){
+    char c = 'z';
+
+    if (c =='a'|| c== 'e'|| c=='i'||c=='o'||c=='u'){
+     System.out.println(c +" "+" is a vowel");
+      }
+    else{
+     System.out.println(c + " "+ "is a consonant");
+      }
+}
+}
