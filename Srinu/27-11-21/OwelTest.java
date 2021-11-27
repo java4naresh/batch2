e297ac0ea3ef1a77f01d192c9950038dbd2e45f1
Jhansi2425 <jhansi1824@gmail.com>
2021-11-28 00:05:57 +0530

    27-11-21 practice added

---
+++ Srinu/27-11-21/OwelTest.java
@@ -0,0 +1,17 @@
+class OwelTest{
+
+public static void main(String[] args){
+// local variable
+char letter ='a';
+ 
+if(letter=='a'){
+System.out.println(letter+" "+"is owel");
+
+}
+else{
+System.out.println(letter+"is not owel");
+
+}
+
+}
+}
---
+++ Srinu/27-11-21/OwelTest2.java
@@ -0,0 +1,20 @@
+class OwelTest2{
+
+public static void main(String[] args){
+
+char letter ='e';
+
+if(letter=='e'){
+
+System.out.println(letter+" "+"is a owel");
+}
+
+else {
+System.out.println(letter+" "+"is not a owel");
+}
+}
+}
+
+
+
+
