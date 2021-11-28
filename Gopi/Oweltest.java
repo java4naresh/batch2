fbc89c906d96bf9d5a6881d092da688fc23b41ad
gopinadh <kgopinadh714316@gmail.com>
2021-11-28 14:29:28 -0500

    Owel Test, Current Bill Test in If and Else if condition is done.

---
+++ Gopi/Oweltest.java
@@ -0,0 +1,15 @@
+class Statement {
+public static void main(String[] args);
+
+char word='a';
+if(word==owel){
+
+System.out.println("owel");
+
+} else{
+
+   System.out.println("not owel");
+
+}
+
+}
