dcfad34f557ba4096410d98d8c11cd88b25443aa
java4naresh <kambalanaresh4java@gmail.com>
2021-12-02 08:35:44 +0530

    2/12/2021 switch case example

---
+++ Naresh/SwitchTest.java
@@ -0,0 +1,65 @@
+class SwitchTest {
+
+public static void main(String[] args) {
+
+
+//int day = 9;
+//String day = "lnkljnn";
+//double number = 1.1;
+char c = 'b';
+//boolean flag = true;
+
+ /* switch(day) {
+default : System.out.println("Invalid Day"); break;
+case 1: System.out.println("Sunday"); break;
+case 2: System.out.println("Monday"); break;
+case 3: System.out.println("Tuesday"); break;
+case 4: System.out.println("Wednesday"); break;
+case 5: System.out.println("Thursday"); break;
+case 6: System.out.println("Friday"); break;
+case 7: System.out.println("Saturday"); break;
+
+
+} */
+
+/* switch(day) {
+default : System.out.println("Invalid Day"); break;
+case "Sunday": System.out.println(1); break;
+case "Monday": System.out.println(2); break;
+case "Tuesday": System.out.println(3); break;
+case "Wednesday": System.out.println(4); break;
+case "Thursday": System.out.println(5); break;
+case "Friday": System.out.println(6); break;
+case "Saturday": System.out.println(7); break;
+
+
+} */
+/*switch((int)1.1) {
+default : System.out.println("Invalid Number"); break;
+case (int)1.1: System.out.println(1.1); break;
+case 1.2: System.out.println(1.2); break;
+case 1.3: System.out.println(1.3); break;
+case 1.4: System.out.println(1.4); break;
+case 1.5: System.out.println(1.5); break;
+case 1.6: System.out.println(1.6); break;
+case 1.7: System.out.println(1.7); break;*/
+
+ switch(c) {
+case 'a': System.out.println("Given Character is Owel"); break;
+case 'e': System.out.println("Given Character is Owel"); break;
+case 'i': System.out.println("Given Character is Owel"); break;
+case 'o': System.out.println("Given Character is Owel"); break;
+case 'u': System.out.println("Given Character is Owel"); break;
+
+}
+
+/*switch(flag) {
+default : System.out.println("Given Value is not boolean"); break;
+case true: System.out.println("Given Character is Owel"); break;
+case false: System.out.println("Given Character is Owel"); break;
+
+} */
+
+}
+
+}
