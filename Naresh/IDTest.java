47901453d8973731fb19a15e3ca26b99c2216b2f
java4naresh <kambalanaresh4java@gmail.com>
2021-12-03 08:58:03 +0530

    3-12-2021 session related commit

---
+++ Naresh/IDTest.java
@@ -0,0 +1,17 @@
+class IDTest {
+
+
+public static void main(String[] args) {
+
+
+int num = 10;
+//++num;
+
+System.out.println(num+++num); //21
+
+System.out.println(num);
+
+}
+
+
+}
---
+++ Naresh/SwitchCase2.java
@@ -0,0 +1,52 @@
+class SwitchCase2 {
+
+public static void main(String[] args) {
+
+int num = 3;
+
+
+switch(num) {
+
+case 1: 
+System.out.println("1*1="+(1*1));
+System.out.println("1*2="+(1*2));
+System.out.println("1*3="+(1*3));
+System.out.println("1*4="+(1*4));
+System.out.println("1*5="+(1*5));
+System.out.println("1*6="+(1*6));
+System.out.println("1*7="+(1*7));
+System.out.println("1*8="+(1*8));
+System.out.println("1*9="+(1*9));
+System.out.println("1*10="+(1*10));
+break;
+case 2: 
+System.out.println("2*1="+(2*1));
+System.out.println("2*2="+(2*2));
+System.out.println("2*3="+(2*3));
+System.out.println("2*4="+(2*4));
+System.out.println("2*5="+(2*5));
+System.out.println("2*6="+(2*6));
+System.out.println("2*7="+(2*7));
+System.out.println("2*8="+(2*8));
+System.out.println("2*9="+(2*9));
+System.out.println("2*10="+(2*10)); 
+break;
+case 3:
+String name = "Naresh";
+if("Naresh".equals(name)) {
+System.out.println("Teacher");
+} else {
+System.out.println("Student");
+}
+System.out.println(name);
+case 4: System.out.println("");
+
+
+}
+
+
+} 
+
+
+
+}
