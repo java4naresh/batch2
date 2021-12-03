fb8a6566f8408a2d8dbdd8ff65962293c7412327
venkateshasul1 <94854961+venkateshasul1@users.noreply.github.com>
2021-12-03 06:34:49 +0530

    Create SwitchCaseTest.java

---
+++ Venkatesh/SwitchCaseTest.java
@@ -0,0 +1,49 @@
+class SwitchCaseTest{
+
+public static void main(String[] args){
+
+//int zodiac = 5;
+//String zodiac= "january";
+char owel='b';
+ /*switch(zodiac) {
+case 1: System.out.println("Aries"); break;
+case 2: System.out.println("Taurus"); break;
+case 3: System.out.println("Gemini"); break;
+case 4: System.out.println("Cancer"); 
+case 5: System.out.println("Leo"); break;
+case 6: System.out.println("Virgo"); break;
+case 7: System.out.println("Libra");  break;
+case 8: System.out.println("Scorpion"); break;
+case 9: System.out.println("Sagitarius"); break;
+case 10: System.out.println("Capricorn"); break;
+case 11: System.out.println("Aquarius"); break;
+case 12: System.out.println("Pisces"); break;
+default: System.out.println("Invalid month");
+}*/
+ /*switch(month){
+case "Aries": System.out.println(1); break;
+case"Taurus": System.out.println(2); break;
+case"Gemini": System.out.println(3); break;
+case "Cancer": System.out.println(4); break;
+case "Leo": System.out.println(5); break;
+case "Virgo": System.out.println(6); break;
+case "Libra": System.out.println(7); break;
+case "Scorpion": System.out.println(8); break;
+case "Sagitarius": System.out.println(9); break;
+case "Capricorn": System.out.println(10); break;
+case "Aquarius": System.out.println(11); break;
+case "Pisces": System.out.println(12); break;
+default :System.out.println("invalid months");
+}*/
+
+switch(owel){
+default :System.out.println("Given character is not owel");break;
+case 'a': System.out.println("Given character is owel "); break;
+case 'e': System.out.println("Given character is owel "); break;
+case 'i': System.out.println("Given character is owel "); break;
+case 'o': System.out.println("Given character is owel "); break;
+case 'u': System.out.println("Given character is owel "); break;
+}
+
+}       
+}
