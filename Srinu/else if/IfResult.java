c843fcca0de4cc4df431cec02e7053186972ba55
Jhansi2425 <jhansi1824@gmail.com>
2021-12-03 18:00:28 +0530

    added

---
+++ Srinu/else if/IfResult.java
@@ -0,0 +1,32 @@
+class IfResult{
+
+public static void main(String[] args){
+
+String name="srinivas";
+
+float percentage=70.25f;
+
+if(percentage>70){
+
+System.out.println(name+" "+"got a first class");
+
+}if(percentage>60&&percentage<70){
+
+System.out.println(name+" "+"got a second class");
+
+}if(percentage>50&&percentage<60){
+
+System.out.println(name+" "+"got a third class");
+
+}if(percentage>40&&percentage<50){
+
+System.out.println(name+" "+"got a fourth class");
+
+}if(percentage<40){
+
+
+System.out.println(name+" "+"got a failed");
+
+}
+}
+}
---
+++ Srinu/else if/OwelTest3.java
@@ -0,0 +1,17 @@
+class OwelTest3{
+
+public static void main(String[] args){
+// local variables
+char letter='e';
+
+if(letter== 'a'||letter== 'e'||letter== 'i'||letter== 'o'||letter== 'u'){
+
+System.out.println(letter+" "+"is a owel");
+}
+
+else {
+  
+System.out.println(letter+" "+"is not a owel");
+}
+}
+}
