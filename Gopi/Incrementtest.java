97e4b7295601cbf74cdd7944bc66a7f5eb9c0df9
gopinadh <kgopinadh714316@gmail.com>
2021-12-03 22:23:41 -0500

    ID TEST

---
+++ Gopi/Incrementtest.java
@@ -0,0 +1,25 @@
+class IncrementTest {
+
+public static void main(String[] args){
+
+int num = 20;
+
+//System.out.println(num++ +num);//41
+
+//System.out.println(++num);//21
+
+//System.out.println(--num);//19
+
+//System.out.println(num--);
+
+//System.out.println(--num +num);//38
+
+System.out.println(num--+num);//39
+
+
+
+}
+
+
+
+}
