9b1a7dcfc3c6a1e24358b95ca8d6dda0877ffc65
venky <venkateshasula1@gmail.com>
2021-12-05 20:46:02 +0530

    ApPolityParties

---
+++ Venkatesh/APParties.java
@@ -0,0 +1,11 @@
+class ApPolityParties{
+public static void main( String[] args)
+{
+ system.out.println("Ap state");
+ system.out.println("Ap jspparty");
+ system.out.println("Ap tdpparty"); 
+ system.out.println("Ap ysrparty");
+ system.out.println("Ap congressparty");
+
+   }
+}
