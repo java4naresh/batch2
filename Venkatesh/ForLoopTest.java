9cf0ad26dd91f50958bb84cffa71a256a0b3e90f
venkateshasul1 <94854961+venkateshasul1@users.noreply.github.com>
2021-12-06 22:40:03 +0530

    Update ForLoopTest.java

--- Venkatesh/ForLoopTest.java
+++ Venkatesh/ForLoopTest.java
@@ -3,7 +3,7 @@ public static void main(String[] args){
 
 
 /*for(int i=1;i<=10;i++){
-System.out.println("gopinadh");
+System.out.println("venkatesh");
 }  for(int i=1;i<=10;i++){
 System.out.println(i);
 }*/  for(int i=1;i<=10;i++){
