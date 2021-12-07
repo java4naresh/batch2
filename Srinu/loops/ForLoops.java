75fadb965af89c00f5f7371ae466bd250ef09c52
Jhansi2425 <jhansi1824@gmail.com>
2021-12-07 11:06:35 +0530

    loops practice

---
+++ Srinu/loops/ForLoops.java
@@ -0,0 +1,78 @@
+class ForLoops{
+
+public static void main(String[] args){
+
+
+//int S=1;
+
+/*for(S=2;S<=20;S++){
+
+
+System.out.println(S);//
+
+}
+
+for(S=1;S<30;S=S+5){
+
+System.out.println(""+S+S+S);//
+
+}
+for(S=1;S<=100;S++){
+
+int rem=S%2;
+
+
+if ( rem==0){
+
+System.out.println(S+" "+"is even num");
+}
+else if(rem!=0){
+System.out.println(S+" "+"is odd num");
+
+
+}
+}
+
+/*int S=1;
+ for(S=1;S<=10;S++){
+
+switch(S){
+
+
+case 1:System.out.println("one");break;
+case 2:System.out.println("two");break;
+case 3:System.out.println("threee");break;
+case 4:System.out.println("four");break;
+case 5:System.out.println("five");break;
+case 6:System.out.println("six");break;
+case 7:System.out.println("seven");break;
+case 8:System.out.println("eight");break;
+case 9:System.out.println("nine");break;
+case 10:System.out.println("ten");break;
+case 11:System.out.println("leven");break;
+case 12:System.out.println("twelve");break;
+default:System.out.println("invaild");
+
+
+
+}
+
+}
+  int t=1;
+for(t=1;t<10;t++){
+
+System.out.println("15*"+t+"="+15*t);
+
+                           
+}*/
+int S=1;
+int num=10;
+int fact=1;
+for(S=1;S<=num;S++){
+fact=fact*S;//6*4
+
+}
+System.out.println(fact);
+}
+
+}
