138779e08d032ac705e2e9e4359776ed18d034ef
venky <venkateshasula1@gmail.com>
2021-12-09 18:31:08 +0530

    9/12/2021

---
+++ Venkatesh/StarPoint.java
@@ -0,0 +1,14 @@
+class starPoint{
+public static void main(string[] args){
+   for(int i=5; i<11; i++){
+   for(int j=1; j<6; j++){
+ System.out.print("*");
+System.out.println();
+ 
+   }
+  
+  }
+ 
+ }
+
+}
