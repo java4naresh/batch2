87e0581ed8fabb2a9e5cbcd187893a8a72b2cf03
Jhansi2425 <jhansi1824@gmail.com>
2021-12-15 02:15:17 +0530

    ternary practice

---
+++ Srinu/ternaryoperators/TernaryOperator.java
@@ -0,0 +1,16 @@
+class TernaryOperator  
+{
+	public static void main(String[] args) 
+	{
+	  /*	String a="s";
+    String names=(a=="s")? "srinu":"gopi";
+	 System.out.println(names);*/
+	/* int num=24;
+	 String rem =(num%2==0)? "even":"odd"; 
+     System.out.println(rem);*/
+	 int num=10;
+	 boolean rem=(num%2!=0)? true:false;
+      System.out.println(rem);
+
+	}
+}
