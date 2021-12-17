feabf03bcc597f28785d78ba521e6fcf040a2b8d
gopinadh <kgopinadh714316@gmail.com>
2021-12-16 21:33:38 -0500

    14/12/21

---
+++ Gopi/14d12m21y/Arraytest1.java
@@ -0,0 +1,15 @@
+class  ArrayTest
+{
+	public static void main(String[] args) 
+	{
+		char chars[] = new char[26];
+		for(int index=0;index<chars.length;index++){
+		 char[] = 'A';
+		
+		
+		}
+           
+			System.out.println("char[index]");
+		
+	}
+}
