b2407fe3df09ac1c3edfe098e15710870cf41047
gopinadh <kgopinadh714316@gmail.com>
2021-12-16 19:34:07 -0500

    add

--- Srinu/array/Array1.java
+++ Srinu/array/Array1.java
@@ -1,15 +1,34 @@
 class Array1 
 {
 	public static void main(String[] args) 
-	{
-		String [] ch=new String[10];
-		for (int index=0;index< ch.length;index++ )
+	{ //(A,B,C,D,.....Z)
+		/*char [] ch=new char[26];
+		for (int i=0;i< ch.length;i++ )
 		{
-			ch[index]= "index + 65";
+			ch[i]= (char)(i+65);
 		}
-       for (int index=0;index< ch.length;index++ ){
-		   System.out.println(ch.length);
-		   System.out.println(ch[index]);
+       for (int i=0;i< ch.length;i++ ){
+		   
+		   System.out.print(ch[i]+ ", ");
+	}  
+	System.out.println(ch.length);*/
+	/*char [] ch=new char[26];
+		for (int i=ch.length-1;i>=0;i-- )
+		{
+			ch[i]= (char)(i+65);
+		}
+       for (int i=ch.length-1;i>=0;i-- ){
+		   
+		   System.out.print(ch[i]+ ", ");*
+	}*/
+	char [] ch=new char[26];
+		for (int i=0;i< ch.length;i++ )
+		{
+			ch[i]= (char)(64+26-i);
+		}
+       for (int i=0;i< ch.length;i++ ){
+		   
+		   System.out.print(ch[i]+ ", ");
 	}
 }
 }
