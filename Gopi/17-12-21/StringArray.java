40fd1959efea493c84569310d46f233dfd5ebf06
gopinadh <kgopinadh714316@gmail.com>
2021-12-20 01:13:52 -0500

    19/12/21 3 task complitated

---
+++ Gopi/17-12-21/StringArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class  StringArray
+{
+	public static void main(String[] args) 
+	{
+		/*String[]  names = new String[5];
+		names[0] = "Gopi";
+		names[1] = "Srinu";
+		names[2] = "Mahi";
+		names[3] = "Kittu";
+		names[4] = "Babu";
+		/*System.out.println(names[0]);
+		System.out.println(names[1]);
+		System.out.println(names[2]);
+		System.out.println(names[3]);
+		System.out.println(names[4]);
+		for(int i=0;i<names.length;i++){
+         System.out.println(Arrays.toString(names));
+        }*/
+		String[] names = {"Gopi","Srinu","Mahi","Kittu","Babu"};
+        System.out.println(Arrays.toString(names));
+         
+	}
+}
