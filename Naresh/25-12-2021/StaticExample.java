6a0cad18860e0fd0b53f4262e2f0b67b49130792
java4naresh <kambalanaresh4java@gmail.com>
2021-12-25 09:31:26 +0530

    25-12-2021 static examples

---
+++ Naresh/25-12-2021/StaticExample.java
@@ -0,0 +1,52 @@
+class StaticExample 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(num);
+        StaticExample se = new StaticExample();
+		se.num2 = 2000;
+		StaticExample se2 = new StaticExample();
+		System.out.println(se.num2);//2000,
+		System.out.println(se2.num2);//1000,
+	}
+
+    static int num = getNum();
+
+	int num2 = getNum2();
+
+	public static int getNum() {
+		System.out.println("getNum");
+	    return 25;
+	}
+
+	public int getNum2() {
+		System.out.println("getNum2");
+	    return 10;
+	} 
+
+	{
+	System.out.println(num2);
+	num2 = 20;
+	
+	}
+
+    {
+	System.out.println(num2);
+	num2 = 1000;
+	
+	}
+
+    
+    
+	static {
+	System.out.println(num);
+	num = 20;
+	}
+
+	static {
+	System.out.println(num);
+	num = 30;
+	}
+
+	
+}
