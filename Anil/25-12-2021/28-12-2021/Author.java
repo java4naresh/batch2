e20b0407e0efee85b205eb5691953c38995c4140
anil4java <ullianil4java@gmail.com>
2021-12-28 23:54:52 +0530

    28-12-2021 of practice

---
+++ Anil/25-12-2021/28-12-2021/Author.java
@@ -0,0 +1,10 @@
+public class Author{
+   public String authorname;
+   public int age;
+   public String place;
+  public Author(String name,int age, String place){
+   authorname= name;
+   age= age;
+   place= place;
+  }
+}
