0e8ae666cb677b1f44365311ba318705d6468d1f
java4naresh <kambalanaresh4java@gmail.com>
2022-01-03 08:58:43 +0530

    03-01-2022 first STS example on inheritence

---
+++ Naresh/03-01-2022/Batch2/src/inheritence2/PrivateExampleClient.java
@@ -0,0 +1,12 @@
+package inheritence2;
+
+import inheritence.PrivateExample;
+
+public class PrivateExampleClient {
+
+	public static void main(String[] args) {
+		//PrivateExample pe = new PrivateExample();
+
+	}
+
+}
