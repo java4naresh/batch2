aba63bd27d17d849a66c326039bded060415cd97
java4naresh <kambalanaresh4java@gmail.com>
2022-01-05 08:40:51 +0530

    5-1-2022 Inheritence Example

--- Naresh/03-01-2022/Batch2/src/inheritence/Father.java
+++ Naresh/03-01-2022/Batch2/src/inheritence/Father.java
@@ -1,6 +1,6 @@
 package inheritence;
 
-public class Father {
+public class Father extends GrandFather{
 	
     String fname;
 	
@@ -10,11 +10,12 @@ public class Father {
 	
 	String surName;
 
-	public Father() {
+	/*public Father() {
 		System.out.println("Father 0 Cons");
-	}
+	}*/
 	
-	public Father(String name, int age, float height, String surName) {
+	public Father(String name, int age, float height, String surName, String gfName, int gfAge, float gfHeight) {
+		super(gfName, gfAge, gfHeight);
 		this.fname = name;
 		this.fage = age;
 		this.fheight = height;
@@ -24,6 +25,16 @@ public class Father {
 	
 	public String getSurName() {
 		return surName;
+	}
+	
+	public String getFDesignation() {
+		return "Businessman";
+	}
+
+	@Override
+	public String toString() {
+		return "Father [fname=" + fname + ", fage=" + fage + ", fheight=" + fheight + ", surName=" + surName
+				+ ", gfName=" + gfName + ", gfAge=" + gfAge + ", gfHeight=" + gfHeight + "]";
 	}
 	
 }
--- Naresh/03-01-2022/Batch2/src/inheritence/SonClient.java
+++ Naresh/03-01-2022/Batch2/src/inheritence/SonClient.java
@@ -3,9 +3,18 @@ package inheritence;
 public class SonClient {
 
 	public static void main(String[] args) {
-       Son s = new Son("Naresh", 30, 5.8f, "Brahmam", 55, 5.5f, "Kambala");
-       String name = s.getName();
-       System.out.println(name);
+       Son s = new Son("Naresh", 30, 5.8f, "Brahmam", 55, 5.5f, "Kambala", "Koteswarao", 90, 5.5f);
+       System.out.println(s);
+       System.out.println(s.getGFDesignation()); 
+       System.out.println(s.getFDesignation());
+       System.out.println(s.getSDesignation());
+       Father f = new Father("Brahmam", 55, 5.5f, "Kambala", "Koteswarao", 90, 5.5f);
+	   System.out.println(f);
+	   System.out.println(f.getFDesignation());
+	   System.out.println(f.getGFDesignation());
+	   GrandFather gf = new GrandFather("Koteswarao", 90, 5.5f);
+	   System.out.println(gf);
+	   System.out.println(gf.getGFDesignation());
 	}
 
 }
