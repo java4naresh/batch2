576ffe12b9545daa9626055a046bc7d4f9717c68
gopinadh <kgopinadh714316@gmail.com>
2022-01-05 20:29:39 -0500

    add

--- Gopi/workbench/java project/src/inheritance/Child1.java
+++ Gopi/workbench/java project/src/inheritance/Child1.java
@@ -7,20 +7,28 @@ public class Child1 extends Son1 {
 	int age1;
 	float height1;
 	char gender1;
-	public Child1(String name1,int age1,float height1,char gender1,String name,int age,float height,char gender,String[] names,int[] ages,float[] heights) {
-		super( name, age,height,gender,names,ages,heights);
+	public Child1(String name1,int age1,float height1,char gender1,String sName,int sAge,float sHeight,char sGender,String[] pNames,int[] pAges,float[] pHeights) {
+		super( sName, sAge,sHeight,sGender,pNames,pAges,pHeights);
 		this.name1 = name1;
 		this.age1 = age1;
 		this.height1 = height1;
 		this.gender1 = gender1;
 		
 	}
-	
+	/*@Override
+	public String toString() {
+		return "Child1 [name=" + name + ", age=" + age + ", height=" + height + ", gender=" + gender + ", names="
+				+ Arrays.toString(names) + ", ages=" + Arrays.toString(ages) + ", heights=" + Arrays.toString(heights)
+				+ "]";
+	}*/
+	@Override
 	public String toString() {
 		return "Child1 [name1=" + name1 + ", age1=" + age1 + ", height1=" + height1 + ", gender1=" + gender1 + ", name="
 				+ name + ", age=" + age + ", height=" + height + ", gender=" + gender + ", names="
 				+ Arrays.toString(names) + ", ages=" + Arrays.toString(ages) + ", heights=" + Arrays.toString(heights)
 				+ "]";
 	}
+	
+	
 
 }
--- Gopi/workbench/java project/src/inheritance/Child2.java
+++ Gopi/workbench/java project/src/inheritance/Child2.java
@@ -8,14 +8,20 @@ public class Child2 extends Son1 {
 	int age2;
 	float height2;
 	char gender2;
-	public Child2(String name2,int age2,float height2,char gender2,String name,int age,float height,char gender,String[] names,int[] ages,float[] heights) {
-		super( name, age,height,gender,names,ages,heights);
+	public Child2(String name2,int age2,float height2,char gender2,String sName,int sAge,float sHeight,char sGender,String[] pNames,int[] pAges,float[] pHeights) {
+		super( sName, sAge,sHeight,sGender,pNames,pAges,pHeights);
 		this.name2 = name2;
 		this.age2 = age2;
 		this.height2 = height2;
 		this.gender2 = gender2;
 		
 	}
+	/*@Override
+	public String toString() {
+		return "Child2 [name=" + name + ", age=" + age + ", height=" + height + ", gender=" + gender + ", names="
+				+ Arrays.toString(names) + ", ages=" + Arrays.toString(ages) + ", heights=" + Arrays.toString(heights)
+				+ "]";
+	}*/
 	@Override
 	public String toString() {
 		return "Child2 [name2=" + name2 + ", age2=" + age2 + ", height2=" + height2 + ", gender2=" + gender2 + ", name="
@@ -23,6 +29,8 @@ public class Child2 extends Son1 {
 				+ Arrays.toString(names) + ", ages=" + Arrays.toString(ages) + ", heights=" + Arrays.toString(heights)
 				+ "]";
 	}
+	
+	
 	
 	
 }
