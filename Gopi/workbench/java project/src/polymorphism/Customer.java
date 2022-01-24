f7de8b4cf61e0159a3834c2d6a426450b83a0f2f
gopinadh <kgopinadh714316@gmail.com>
2022-01-24 00:16:53 -0500

    polymorphism examples

---
+++ Gopi/workbench/java project/src/polymorphism/Customer.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class Customer {
+
+	public static void main(String[] args) {
+		Emp emp = new Emp();
+		emp.salary();
+		EmpTest et = new EmpTest();
+		et.salary();
+
+	}
+
+}
---
+++ Gopi/workbench/java project/src/polymorphism/Emp.java
@@ -0,0 +1,8 @@
+package polymorphism;
+
+public class Emp {
+
+	public void salary() {
+		System.out.println("10,000");
+	}
+}
