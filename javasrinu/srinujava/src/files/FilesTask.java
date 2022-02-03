0acb469483c9a4369f0a83a6c8921aa8344e4b40
Jhansi2425 <jhansi1824@gmail.com>
2022-02-03 07:46:32 +0530

    03-02-22

--- javasrinu/srinujava/src/files/FilesTask.java
+++ javasrinu/srinujava/src/files/FilesTask.java
@@ -3,7 +3,9 @@ package files;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 public class FilesTask {
@@ -79,12 +81,58 @@ public class FilesTask {
 	public static void main(String[] args) {
 
 		
-		
        String fileName = "C:\\\\Users\\\\ADMIN\\\\Desktop\\\\simple.txt";
-		FileReader fr;
+
 		try {
 			File file = new File(fileName);
-			fr = new FileReader(file);
+			FileWriter	fw = new FileWriter(file,true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.write("Srinu");
+			pw.write(" Thirumalasetti");
+			pw.print( " " +'M');
+			pw.print(" " + 25);
+			pw.print(" " + 10000);
+			pw.print( " "+5.5f);
+			pw.println();
+			pw.write("Naresh");
+			pw.write(" Kambala");
+			pw.print( " " +'M');
+			pw.print(" " + 30);
+			pw.print(" " + 50000);
+			pw.print( " "+5.7f);
+			pw.println();
+			pw.write("Gopi");
+			pw.write(" Kambala");
+			pw.print( " " +'M');
+			pw.print(" " + 25);
+			pw.print(" " + 10000);
+			pw.print( " "+5.5f);
+			pw.println();
+			pw.write("Anil");
+			pw.write(" ulli");
+			pw.print( " " +'M');
+			pw.print(" " + 28);
+			pw.print(" " + 300000);
+			pw.print( " "+5.7f);
+			pw.println();
+			pw.write("Venki");
+			pw.write(" Asula");
+			pw.print( " " +'M');
+			pw.print(" " + 28);
+			pw.print(" " + 30000);
+			pw.print( " "+5.7f);
+			pw.println();
+			pw.flush();
+			System.out.println("created");
+		} catch (IOException e) {
+	
+			e.printStackTrace();
+		}
+		
+	
+		try {
+			File file = new File(fileName);
+			FileReader fr = new FileReader(file);
 			BufferedReader br = new BufferedReader(fr);
 			/*String name = br.readLine();S
 			System.out.println(name);*/
@@ -97,6 +145,7 @@ public class FilesTask {
 			System.out.println(Arrays.toString(array));
 			
 			FilesTask ft = new FilesTask();
+			
 			ft.setFastName(array[0]);
 			ft.setLastName(array[1]);
 			ft.setGender(array[2].charAt(0));
