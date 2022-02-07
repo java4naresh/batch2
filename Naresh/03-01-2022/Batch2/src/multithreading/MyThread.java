4dcadbd0c134eac48960328ccb99f14edb5bb1d9
java4naresh <kambalanaresh4java@gmail.com>
2022-02-07 08:39:05 +0530

    Multithreading examples

--- Naresh/03-01-2022/Batch2/src/multithreading/MyThread.java
+++ Naresh/03-01-2022/Batch2/src/multithreading/MyThread.java
@@ -4,6 +4,15 @@ public class MyThread extends Thread {
 	
 	@Override
 	public void run() {
+		/*if("MyThread".equals(Thread.currentThread().getName())) {
+			//Thread.yield();
+			try {
+				Thread.currentThread().join(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}*/
+			
 		for(int i=0; i<=1000; i++) {
 			System.out.println(i+" "+ currentThread().getName());
 		}
--- Naresh/03-01-2022/Batch2/src/multithreading/ThreadClient.java
+++ Naresh/03-01-2022/Batch2/src/multithreading/ThreadClient.java
@@ -4,13 +4,24 @@ public class ThreadClient {
 
 	public static void main(String[] args) {
 
+		System.out.println(Thread.currentThread().getPriority());//Main Thread Priority
 		MyThread mt = new MyThread();
 		mt.setName("MyThread");
+        System.out.println(mt.getPriority());
+		//mt.setPriority(1);
+		//System.out.println(mt.getPriority());
 		mt.start();//it will register new thread into Thread Scheduler and execute run method
-	    //mt.start();
+	    try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		//mt.start();
 		MyThread mt2 = new MyThread();
 	    mt2.setName("MyThread2");
+	    mt2.setPriority(10);
 	    mt2.start();
+	    System.out.println(mt2.getPriority());
 		mt.run();//method calling from main thread
 	}
 
