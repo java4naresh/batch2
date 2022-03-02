b8171ca461fb404dd7cb3e5cda0df08df1d81b28
gopinadh <kgopinadh714316@gmail.com>
2022-03-03 00:18:48 +0530

    add

--- Gopi/workbench/java project/workbench/src/com/gopi/jdbc/JDBCapp1.java
+++ Gopi/workbench/java project/workbench/src/com/gopi/jdbc/JDBCapp1.java
@@ -12,7 +12,7 @@ public class JDBCapp1 {
 			Class.forName("com.mysql.jdbc.Driver"); //driver register
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Gopi", "root", "root");
 			Statement stmt = con.createStatement();
-		//	stmt.executeUpdate("create table Employee (emp_id integer,emp_name varchar(25),emp_gender char)");
+			stmt.executeUpdate("create table Employee (emp_id integer,emp_name varchar(25),emp_gender char)");
 			String query = "insert into Employee values(1,"+"'"+"Gopi"+"'"+","+"'"+"M"+"'"+")";
 			System.out.println(query);
 			stmt.executeUpdate(query);
